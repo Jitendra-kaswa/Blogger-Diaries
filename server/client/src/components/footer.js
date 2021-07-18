00f00e1e97942700399d80d3f67debea66e72a5a
Jitendra-kaswa <kaswanjitendra89@gmail.com>
2021-07-18 12:57:17 +0530

    v3

---
+++ server/client/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">@2021 Jitendra Kaswa</span>
+      </div>
+    </footer>
+  );
+}
--- server/server.js
+++ server/server.js
@@ -6,24 +6,27 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
-const mongoose = require('mongoose');
 const connectDB= require('./config/mongoose')
-// const cors = require('cors');  // we don't need it anymore, because we use proxy server instead
+const cors = require('cors');
+const port = process.env.PORT || 3090;
 
 // DB Setup (connect mongoose and instance of mongodb)
-
-//mongoose.connect('mongodb://localhost:blog/blog');
 connectDB();
+const arr = express();
 // App Setup (morgan and body-parser are middleware in Express)
 app.use(morgan('combined'));  // middleware for logging
 app.use(bodyParser.json({ type: '*/*' }));  // middleware for helping parse incoming HTTP requests
-// app.use(cors());  // middleware for circumventing (规避) cors error
+app.use(cors());  // middleware for circumventing (规避) cors error
 
-// Router Setup
-router(app);
-
-// Server Setup
-const port = process.env.PORT || 3090;
-const server = http.createServer(app);
-server.listen(port);
+// // Router Setup
+// router(app);
+// // Server Setup
+// const server = http.createServer(app);
+app.use('/',router);
+// 3. heroku Setup
+if(process.env.NODE_ENV == 'production'){
+    app.use(express.static("client/build"));
+}
+app.listen(port);
+// server.listen(port);
 console.log('Server listening on: ', port);
